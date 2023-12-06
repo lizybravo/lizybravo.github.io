eaa152f1e7c41fe26115da6fb6fcfea88d96eefa
lizybravo <lizeth.bravo.a@gmail.com>
2023-12-06 06:31:12 -0600

    Se agregan modificaciones al modal. Validaciones de formulario y animaciones

--- js/main.js
+++ js/main.js
@@ -1,23 +1,101 @@
 
-const images = document.querySelectorAll(".animacion_element");
+//animaciones
+const seccionesOcultas1 = document.querySelectorAll('.hidden1');
+const seccionesOcultas2 = document.querySelectorAll('.hidden2');
+const seccionesOcultas3 = document.querySelectorAll('.hidden3');
 
-function triggerAnimation(entries){
-    entries.forEach(entry =>{
-        const image = entry.target.querySelectorAll("img");
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
 
-        image.classList.toggle("unset", entry.isIntersecting);
+        entry.target.classList.toggle('mostrar', entry.isIntersecting); 
     });
+},
+
+{threshold: .8}
+);
+
+seccionesOcultas1.forEach((seccion)=>observer.observe(seccion));
+seccionesOcultas2.forEach((seccion)=>observer.observe(seccion));
+seccionesOcultas3.forEach((seccion)=>observer.observe(seccion));
+
+//validacion formulario
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{5,40}$/, // Letras y espacios, pueden llevar acentos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^(?!.*(\d)\1{4})\d{10}$/,
 }
 
-const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 1
+
+const campos = {
+	nombre: false,
+	correo: false,
+	teléfono: false,
+	mensaje: false,
 }
 
-const observer = new InteractionObserver(triggerAnimation, options);
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
 
-images.forEach(image =>{
-    observer.observe(image);
-})
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'correo');
+		break;
+
+		case "telefono":
+			validarCampo(expresiones.telefono, e.target, "telefono")
+
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}
+}
+
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});
+
+
+
+const btn = document.getElementById('button');
+
+document.getElementById('formulario')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_4rh735s';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Enviar';
+      alert('Enviado');
+    }, (err) => {
+      btn.value = 'Enviar';
+      alert(JSON.stringify(err));
+    });
+});
 
